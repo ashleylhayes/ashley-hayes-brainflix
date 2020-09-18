9db37d31a957ceb97bd731e794102e01f55b6074
Ashley Hayes <ashley_hayes@hotmail.ca>
2020-09-18 16:19:22 -0600

    re-organized components again, and finished styling header for all breakpoints

--- sprint-1/src/components/Header/Header.jsx
+++ sprint-1/src/components/Header/Header.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import './header.scss';
 import logo from '../../assets/Logo/Logo-brainflix.svg';
-import SearchBar from './SearchBar';
-import UploadButton from './UploadButton';
+import searchIcon from '../../assets/Icons/SVG/Icon-search.svg';
+import uploadIcon from '../../assets/Icons/SVG/Icon-upload.svg';
 
 function Header() {
     return (
         <div className='header'>
             <img className='header__logo' src={logo} alt='logo with blue square with a white arrow inside, representing a play button. The word BrainFlix is written in black to the right of that.' />
-            <SearchBar />
-            <UploadButton />
+            <input type="search" src={searchIcon} name="search" placeholder="Search" className='header__searchbar'/>
+            <button className='header__upload-button'>
+                <img className='header__upload-icon' src={uploadIcon} alt='plus sign icon'/>
+                <p className='header__upload-text'>UPLOAD</p>
+            </button>
             <div className='header__avatar-container'></div>
         </div>
     )
