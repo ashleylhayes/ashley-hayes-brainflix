48f8d6eb30b42f0e459f6fd9c12053f14327a447
Ashley Hayes <ashley_hayes@hotmail.ca>
2020-09-19 15:21:12 -0600

    styled current video and controls for tablet breakpoint

--- sprint-1/src/components/CurrentVideo/CurrentVideo.jsx
+++ sprint-1/src/components/CurrentVideo/CurrentVideo.jsx
@@ -7,22 +7,22 @@ import volumeIcon from '../../assets/Icons/SVG/Icon-volume.svg';
 
 function CurrentVideo() {
     return (
-        <div className='current-video'>
-            <video className='current-video__video' poster={videoList0}></video>
+        <section className='current-video'>
+        <video className='current-video__video' poster={videoList0}></video>
             <div className='current-video__controls-container'>
                 <div className='current-video__play'>
-                    <img className='current-video__play-icon' src={playIcon} />
+                    <img className='current-video__play-icon' src={playIcon} alt='play arrow icon' />
                 </div>
                 <div className='current-video__scrubber'>
                     <div className='current-video__scrubber-line'></div>
-                    <p className='current-video__scrubber-time'></p>
+                    <p className='current-video__scrubber-time'>0:00/0:42</p>
                 </div>
                 <div className='current-video__expand-volume'>
-                    <img className='current-video__expand' src={expandIcon} />
-                    <img className='current-video__volume' src={volumeIcon} />
+                    <img className='current-video__expand' src={expandIcon} alt='expand video icon' />
+                    <img className='current-video__volume' src={volumeIcon} alt='volume icon' />
                 </div>
             </div>
-        </div>
+        </section>
     )
 }
 
