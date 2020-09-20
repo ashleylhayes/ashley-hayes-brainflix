ca30fb51c78c94318148a0e12779c8be9177c2e1
Ashley Hayes <ashley_hayes@hotmail.ca>
2020-09-20 01:23:32 -0600

    added state to store data for main video, up to description, and passed as props

--- sprint-1/src/components/CurrentVideo/CurrentVideo.jsx
+++ sprint-1/src/components/CurrentVideo/CurrentVideo.jsx
@@ -8,17 +8,33 @@ import viewsIcon from '../../assets/Icons/SVG/Icon-views.svg';
 import likesIcon from '../../assets/Icons/SVG/Icon-likes.svg';
 
 class CurrentVideo extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            video: videoList0,
+            duration: '0:42',
+            title: 'BMX Rampage: 2018 Highlights',
+            channel: 'Red Cow',
+            timestamp: '12/18/2018',
+            viewsImage: viewsIcon,
+            views: '1,001,023',
+            likesImage: likesIcon,
+            likes: '110,985',
+            description: 'On a gusty day in Southern Utah, a group of 25 daring mountain bikers blew the doors off what is possible on two wheels, unleashing some of the biggest moments the sport has ever seen. While mother nature only allowed for one full run before the conditions made it impossible to ride, that was all that was needed for event veteran Kyle Strait, who won the event for the second time -- eight years after his first Red Cow Rampage title',
+
+        };
+    }
     render() {
         return (
             <section className='current-video'>
-                <video className='current-video__video' poster={videoList0}></video>
+                <video className='current-video__video' poster={this.state.video}></video>
                 <div className='current-video__controls-container'>
                     <div className='current-video__play'>
                     <img className='current-video__play-icon' src={playIcon} alt='play arrow icon' />
                     </div>
                     <div className='current-video__scrubber'>
                         <div className='current-video__scrubber-line'></div>
-                        <p className='current-video__scrubber-time'>0:00 / 0:42</p>
+                        <p className='current-video__scrubber-time'>0:00 / {this.state.duration}</p>
                     </div>
                     <div className='current-video__expand-volume'>
                         <img className='current-video__expand' src={expandIcon} alt='expand video icon' />
@@ -26,25 +42,27 @@ class CurrentVideo extends React.Component {
                     </div>
                 </div>
                 <div className='current-video__details'>
-                    <h1 className= 'current-video__title'>BMX Rampage: 2018 Highlights</h1>
-                    <p className='current-video__uploader'>By Red Cow</p>
-                    <p className='current-video__date'>12/18/2018</p>
-                    <TallyComponent src={viewsIcon} tally={"1,001,023"} />
-                    <TallyComponent src={likesIcon} tally={"110,985"} />
+                    <h1 className= 'current-video__title'>{this.state.title}</h1>
+                    <p className='current-video__uploader'>By {this.state.channel}</p>
+                    <p className='current-video__date'>{this.state.timestamp}</p>
+                    <div className='current-video__tally-container'>
+                        <img className='current-video__tally-icon' src={this.state.viewsImage} alt="" />
+                        <p className='current-video__tally-text'>{this.state.views}</p>
+                    </div>
+                    <div className='current-video__tally-container'>
+                        <img className='current-video__tally-icon' src={this.state.likesImage} alt="" />
+                        <p className='current-video__tally-text'>{this.state.likes}</p>
+                    </div>
                 </div>
                 <div className='current-video__description-container'>
-                    <p className='current-video__description'>On a gusty day in Southern Utah, a group of 25 daring mountain bikers blew the doors off what is possible on two wheels, unleashing some of the biggest moments the sport has ever seen. While mother nature only allowed for one full run before the conditions made it impossible to ride, that was all that was needed for event veteran Kyle Strait, who won the event for the second time -- eight years after his first Red Cow Rampage title</p>
+                    <p className='current-video__description'>{this.state.description}</p>
+                </div>
+                <div className='current-video__comments-container'>
+                    <p className='current-video__comments-number'>3 Comments</p>
                 </div>
             </section>
         );
     }
 }
-
-function TallyComponent(props) {
-    return (
-    <div className='current-video__tally-container'> <img className='current-video__tally-icon' src={props.icon} alt=" " /> <p className='current-video__tally-text'>{props.tally}</p> </div>
-    );
-}
-
 
 export default CurrentVideo;
