cd0e9a6fcb4ad779e7879f86d6e77271363dc462
Ashley Hayes <ashley_hayes@hotmail.ca>
2020-09-22 23:17:36 -0600

    re-styled upload button after adding link

--- sprint-2/src/components/Header/Header.jsx
+++ sprint-2/src/components/Header/Header.jsx
@@ -8,15 +8,17 @@ import uploadIcon from '../../assets/Icons/SVG/Icon-upload.svg';
 function Header() {
     return (
         <header className='header'>
-            <Link to="/" className="header__logo-link">
-            <img className='header__logo' src={logo} alt='logo with blue square with a white arrow inside, representing a play button. The word BrainFlix is written in black to the right of that.' />
-            </Link>
+            <div>
+                <Link to="/" className="header__logo-link">
+                    <img className='header__logo' src={logo} alt='logo with blue square with a white arrow inside, representing a play button. The word BrainFlix is written in black to the right of that.' />
+                </Link>
+            </div>
             <input type="search" src={searchIcon} name="search" placeholder="Search" className='header__searchbar'/>
             <button className='header__upload-button'>
-            <Link to="/videoupload" className="header__upload-button-link">
-                <img className='header__upload-icon' src={uploadIcon} alt='plus sign icon'/>
-                <p className='header__upload-text'>UPLOAD</p>
-            </Link>
+                <Link to="/videoupload" className="header__upload-button-link">
+                    <img className='header__upload-icon' src={uploadIcon} alt='plus sign icon'/>
+                    <p className='header__upload-text'>UPLOAD</p>
+                </Link>
             </button>
             <div className='header__avatar-container'></div>
         </header>
