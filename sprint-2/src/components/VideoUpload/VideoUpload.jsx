31d9fc6a4cdb5aeb58f7a9b7da7c0a8ff0bf2adc
Ashley Hayes <ashley_hayes@hotmail.ca>
2020-09-24 16:32:21 -0600

    continued creating VideoUpload page and styling

--- sprint-2/src/components/VideoUpload/VideoUpload.jsx
+++ sprint-2/src/components/VideoUpload/VideoUpload.jsx
@@ -10,12 +10,17 @@ class VideoUpload extends Component {
                 <div>
                     <Header />
                 </div>
+                <div className='upload-video__divider'></div>
                 <div className="upload-video">
                     <h1 className="upload-video__title">Upload Video</h1>
                     <p className="upload-video__thumbnail-title">VIDEO THUMBNAIL</p>
                     <div className="upload-video__thumbnail-container">
                         <img className="upload-video__thumbnail" src={uploadThumbnail} alt="" />
                     </div>
+                    <form className="upload-video__form">
+                        <p className="upload-video__video-title-title">TITLE YOUR VIDEO</p>
+                        <input className="upload-video__video-title-input" placeholder="Add a title to your video"></input>
+                    </form>
                 </div>
             </div>
         )
