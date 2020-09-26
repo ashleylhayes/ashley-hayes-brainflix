6830dd5950901a8fd6e50d0c6b19996d507222d3
Ashley Hayes <ashley_hayes@hotmail.ca>
2020-09-26 16:02:50 -0600

    successfully populated comments with data from api, deleted NextVideo component to have easier access to all data within the same state

--- sprint-2/src/components/CurrentVideo/CurrentVideo.jsx
+++ sprint-2/src/components/CurrentVideo/CurrentVideo.jsx
@@ -3,7 +3,7 @@ import './currentVideo.scss';
 import Header from '../Header/Header';
 import CommentsForm from '../CommentsForm/CommentsForm'
 import Comment from '../Comment/Comment'
-import NextVideo from '../NextVideo/NextVideo';
+import SideVideo from '../SideVideo/SideVideo';
 import playIcon from '../../assets/Icons/SVG/Icon-play.svg';
 import expandIcon from '../../assets/Icons/SVG/Icon-fullscreen.svg';
 import volumeIcon from '../../assets/Icons/SVG/Icon-volume.svg';
@@ -12,27 +12,38 @@ import likesIcon from '../../assets/Icons/SVG/Icon-likes.svg';
 import axios from 'axios';
 
 const API_KEY = "?api_key=6b0f89c7-4033-408f-b97d-1440f9c4b0c4";
-const VIDEOS_URL = "https://project-2-api.herokuapp.com/videos/1af0jruup5gu";
+const VIDEO_URL = "https://project-2-api.herokuapp.com/videos/1af0jruup5gu";
+const VIDEOS_URL = "https://project-2-api.herokuapp.com/videos";
 
 class CurrentVideo extends React.Component {
     state = {
-        currentVideoContent: []
+        currentVideoContent: [],
+        commentsContent: [],
+        sideVideoContent: []
     };
 
     componentDidMount() {
+        axios.get(VIDEO_URL + API_KEY).then(({ data }) => {
+            console.log(data);
+            console.log(data.comments);
+            this.setState({
+                currentVideoContent: data,
+                commentsContent: data.comments
+            })
+        })
+
         axios.get(VIDEOS_URL + API_KEY).then(({ data }) => {
             console.log(data);
             this.setState({
-                currentVideoContent: data
+                sideVideoContent: data
             })
-            console.log(this.state.currentVideoContent.comments);
         })
     }
 
     render() {
         return (
             <div>
-                <Header />
+            <Header />
             <section className='current-video'>
                 <video className='current-video__video' poster={this.state.currentVideoContent.image}></video>
                 <div className='current-video__controls-container'>
@@ -69,11 +80,16 @@ class CurrentVideo extends React.Component {
                         <div className='current-video__comments-container'>
                             <p className='current-video__comments-number'>3 Comments</p>
                             <CommentsForm />
-                            {/* {this.state.currentVideoContent.comments.map((content) => <Comment key={content.id} name={content.name} date={content.timestamp} comment={content.comment} />)} */}
+                            {this.state.commentsContent.map((content) => <Comment key={content.id} name={content.name} date={content.timestamp} comment={content.comment} />)}
                         </div>
                     </div>
                     <div>
-                        <NextVideo />
+                        <section className='next-video__container'>
+                            <p className='next-video__title'>NEXT VIDEO</p>
+                            <div>
+                                {this.state.sideVideoContent.map((content) => <SideVideo key={content.id} image={content.image} title={content.title} channel={content.channel} />)}
+                            </div>
+                        </section>
                     </div>
                 </main>
             </section>
